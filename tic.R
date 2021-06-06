a8ec764fa5cee0648159947402d4e0cd64f0e8ac
pat-s <patrick.schratz@gmail.com>
2021-06-06 20:19:54 +0200

    now?

--- tic.R
+++ tic.R
@@ -1,4 +1,6 @@
 get_stage("deploy") %>%
-  add_step(step_install_cran(c("bslib", "downlit", "xml2")))
+  add_step(step_install_cran("bslib")) %>%
+  add_step(step_install_cran("downlit")) %>%
+  add_step(step_install_cran("xml2"))
 
 do_bookdown()
